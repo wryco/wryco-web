412d5a46a0fc02bcd386393dfd6ac40eda48ea86
ramity <lewisclbrown@gmail.com>
2025-06-20 17:29:48 -0500

    changed port mappings, added vue router, minified App.vue, example IndexView.vue, added hotreload functionality

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import IndexView from './views/IndexView.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: IndexView },
+    ]
+});
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/',
+  configureWebpack: {
+    entry: "./src/main.js",  
+    devServer: {
+      hot: true,
+      port: 8080,
+      host: '0.0.0.0',
+    },
+    watchOptions: {
+      ignored: /node_modules/,
+      poll: 1000,
+    }
+  }
 })
