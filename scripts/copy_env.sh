693257af10d4042f8c393066987f8fc79ea6c169
ramity <lewisclbrown@gmail.com>
2025-06-19 23:25:04 -0500

    added nginx service, updated copy_env script, added log dir, updated .gitignore, docker-compose comments, reproxy support

--- scripts/copy_env.sh
+++ scripts/copy_env.sh
@@ -5,3 +5,6 @@ cp docker/adminer/.env docker/adminer/.env.local
 cp docker/backend/.env docker/backend/.env.local
 cp docker/frontend/.env docker/frontend/.env.local
 cp docker/mssql/.env docker/mssql/.env.local
+
+cp docker/nginx/caspnetti-ssl.conf docker/nginx/caspnetti-ssl.conf.local
+cp docker/nginx/caspnetti.conf docker/nginx/caspnetti.conf.local
