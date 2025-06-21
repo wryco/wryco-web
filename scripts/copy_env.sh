9caad1b148668487ba8135108b584923d87136d3
ramity <lewisclbrown@gmail.com>
2025-06-21 00:46:24 -0500

    added bin & obj to .dockerignore, abstracted backend and nginx to separate dev and prod services, updated configs to match, updated copy_env and recreate scripts, added copytopublishdirectory itemgroup to API project definition for assets directory

--- scripts/copy_env.sh
+++ scripts/copy_env.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Copy sample .env files
-cp docker/adminer/.env docker/adminer/.env.local
-cp docker/backend/.env docker/backend/.env.local
-cp docker/mssql/.env docker/mssql/.env.local
-
-cp docker/nginx/caspnetti-ssl.conf docker/nginx/caspnetti-ssl.conf.local
-cp docker/nginx/caspnetti.conf docker/nginx/caspnetti.conf.local
-
-cp docker/vue_development/.env docker/vue_development/.env.local
-cp docker/vue_production/.env docker/vue_production/.env.local
+# Copy default .env if .env.local does not already exist for all services:
+cp -n docker/adminer/.env docker/adminer/.env.local
+cp -n docker/aspnet_development/.env docker/aspnet_development/.env.local
+cp -n docker/aspnet_production/.env docker/aspnet_production/.env.local
+cp -n docker/mssql/.env docker/mssql/.env.local
+cp -n docker/nginx_development/caspnetti-ssl.conf docker/nginx_development/caspnetti-ssl.conf.local
+cp -n docker/nginx_development/caspnetti.conf docker/nginx_development/caspnetti.conf.local
+cp -n docker/nginx_production/caspnetti-ssl.conf docker/nginx_production/caspnetti-ssl.conf.local
+cp -n docker/nginx_production/caspnetti.conf docker/nginx_production/caspnetti.conf.local
+cp -n docker/vue_development/.env docker/vue_development/.env.local
+cp -n docker/vue_production/.env docker/vue_production/.env.local
--- scripts/recreate.sh
+++ scripts/recreate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# Recreates all containers, images, networks, preserving derived images
 docker compose down --rmi local
 docker compose up -d
